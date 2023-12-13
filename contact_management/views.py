aa6b967771acd2afc9d0fca827f2c61383362ccc
DESKTOP-L2BAH6G\saipr <saipreethi.poka@gmail.com>
2023-12-12 23:45:18 -0800

    changes w.r.t detail page

--- contact_management/views.py
+++ contact_management/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.views.generic import ListView, CreateView, DetailView
 from .models import Contact
 from .forms import ContactForm
 
@@ -15,4 +15,8 @@ class ContactCreateView(CreateView):
     template_name = 'Contact_form.html'
     form_class = ContactForm
     success_url = reverse_lazy('contatc_management:contact_list')
+
+class ContactDetailView(DetailView):
+    model = Contact
+    template_name = 'contact_detail.html'
 
