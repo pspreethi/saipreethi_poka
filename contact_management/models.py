15c1e59d74434a705c33f865d156502494f840e3
DESKTOP-L2BAH6G\saipr <saipreethi.poka@gmail.com>
2023-12-13 00:45:16 -0800

    Changes w.r.t the contact lists

--- contact_management/models.py
+++ contact_management/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Contact(models.Model):
     name = models.CharField(max_length=255, unique=True)
     email = models.EmailField(unique=True)
     notes = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField( auto_now_add=True)
     
     def __str__(self):
         return self.name
