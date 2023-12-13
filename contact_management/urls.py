2b8d76ea611d2f62d5b651a6fca754140bda3fad
DESKTOP-L2BAH6G\saipr <saipreethi.poka@gmail.com>
2023-12-12 23:35:06 -0800

    Changes w.r.t to detail page

--- contact_management/urls.py
+++ contact_management/urls.py
@@ -1,10 +1,11 @@
 # Contact_management/urls.py
 from django.urls import path
-from .views import ContactListView, ContactCreateView
+from .views import ContactListView, ContactCreateView, ContactDetailView
 
 app_name = 'contact_management'
 
 urlpatterns = [
     path('', ContactListView.as_view(), name='contact_list'),
     path('new/', ContactCreateView.as_view(), name='contact_create'),
+    path('<int:pk>/', ContactDetailView.as_view(), name='contact_detail'),
 ]
