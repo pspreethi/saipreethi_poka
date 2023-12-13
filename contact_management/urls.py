59699cb55a61009c9108dcad1900ffee3804abee
DESKTOP-L2BAH6G\saipr <saipreethi.poka@gmail.com>
2023-12-13 00:03:32 -0800

    changes w.r.t update

--- contact_management/urls.py
+++ contact_management/urls.py
@@ -1,6 +1,8 @@
 # Contact_management/urls.py
+
 from django.urls import path
-from .views import ContactListView, ContactCreateView, ContactDetailView
+from .views import ContactListView, ContactCreateView, ContactUpdateView, ContactDetailView
+
 
 app_name = 'contact_management'
 
@@ -8,4 +10,5 @@ urlpatterns = [
     path('', ContactListView.as_view(), name='contact_list'),
     path('new/', ContactCreateView.as_view(), name='contact_create'),
     path('<int:pk>/', ContactDetailView.as_view(), name='contact_detail'),
+    path('<int:pk>/edit/', ContactUpdateView.as_view(), name='contact_edit'),
 ]
